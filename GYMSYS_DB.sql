17e96bf3d744eac9d9e5e7a87b9bc377eb51cbe1
EoinMannix <eoinmannix83@gmail.com>
2026-02-02 16:35:21 +0000

    Completion of table creation
    Next initiative is to Populate the tables with sample data.

--- GYMSYS_DB.sql
+++ GYMSYS_DB.sql
@@ -42,5 +42,76 @@ CREATE TABLE Members
 
 -- Instructors Table
 
+PROMPT CREATING Table Instructors
+PROMPT
+
+CREATE TABLE Instructors
+(
+    InstructorID NUMERIC(4),
+    Forename VARCHAR(20),
+    Surename VARCHAR2(20),
+    DOB DATE,
+    Phone NUMERIC(10),
+    Email VARCHAR2(40),
+    Gender VARCHAR2(6),
+    
+    CONSTRAINT pk_Instructors PRIMARY KEY (InstructorID)
+);
+
+
+-- Classes Table
+
+PROMPT CREATING Table Classes
+PROMPT
+
+CREATE TABLE Classes
+(
+    ClassID NUMERIC(4),
+    ClassName VARCHAR2(20),
+    InstructorID NUMERIC(4),
+    TypeCode CHAR(2),
+    ClassDate DATE,
+    ClassTime VARCHAR2(5), -- I put varchar as there is then room for a semi colon eg 10:00
+    Price NUMERIC(6,2),
+    Room VARCHAR2(10),
+    Status VARCHAR(10),
+    
+    CONSTRAINT pk_Classes PRIMARY KEY (ClassID),
+    
+    CONSTRAINT fk_Classes_Instructors
+        FOREIGN KEY (InstructorID) REFERENCES Instructors,
+        
+    CONSTRAINT fk_Classes_Types
+        FOREIGN KEY (TypeCode)  REFERENCES ClassTypes
+);
+
+-- Booking table 
+
+PROMPT CREATING Table Bookings
+PROMPT
+
+CREATE TABLE Bookings 
+(
+    BookingID NUMERIC(4),
+    MemberID NUMERIC(4),
+    ClassID CHAR(4),
+    BookingDate DATE,
+    Status VARCHAR2(10),
+    
+    CONSTRAINT pk_Bookings PRIMARY KEY (BookingID),
+    
+    CONSTRAINT fk_Bookings_Members
+        FOREIGN KEY (MemberID) REFERENCES Members,
+        
+    CONSTRAINT fk_Bookings_Classes
+        FOREIGN KEY (ClassID) REFERENCES Classes
+);
+
+
+    
+    
+    
+    
+
 
 
