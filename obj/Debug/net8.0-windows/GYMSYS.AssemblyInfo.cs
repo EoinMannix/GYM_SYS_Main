dbd4e9420e71e2f1dbb9e73426dc95a0ec8f7952
EoinMannix <eoinmannix83@gmail.com>
2026-05-01 20:12:28 +0100

    Nearing the End of my server side project
    Mad quite a few changes across the board such as validation, better code logic and a report that documents how I went through the project.

--- obj/Debug/net8.0-windows/GYMSYS.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/GYMSYS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GYMSYS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f461ec7ef2284969c3ad223420ddedac23634df1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+60a6b7a79d0f4b17b72d68a6b5bf5ff2c1faea5a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GYMSYS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GYMSYS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
